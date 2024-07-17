6bf6fbdfc9910aa4e0ae113458cd2b5a61ac2467
pastorAlexanderCastro <pastorcastro237@gmail.com>
2024-07-16 22:01:33 -0400

    Mejoria de estilos

--- index.js
+++ index.js
@@ -1,14 +1,22 @@
 const exchange__seccion=document.querySelector(".exchange__seccion--table")
 const arrow_right=document.querySelector(".arrow__right")
 const arrow_left=document.querySelector(".arrow__left")
+const flecha_derecha=document.querySelector(".flecha_derecha")
+const flecha_izquierda=document.querySelector(".flecha_izquieda")
+const container_card=document.querySelector(".container_card")
 
 
 
 arrow_right.addEventListener("click",()=>{
-    exchange__seccion.classList.remove("carrucel--100")
+    
     exchange__seccion.classList.add("carrucel--0")
 })
 arrow_left.addEventListener("click",()=>{
     exchange__seccion.classList.remove("carrucel--0")
     exchange__seccion.classList.toggle("carrucel--100")
+})
+
+
+flecha_izquierda.addEventListener("click",()=>{
+    container_card.classList.add("move_lept")
 })
